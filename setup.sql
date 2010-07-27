8f760e81abe93c1f869ed9a830ee5780daf8ed48
John Hobbs <john@velvetcache.org>
2010-07-27 00:03:55 -0500

    More thinking about how the flow works, more work to data types and added comments.

--- setup.sql
+++ setup.sql
@@ -1,75 +1,106 @@
+/*
+	A user is a user is a user.
+*/
 CREATE TABLE `users` (
 	`id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-	`joined` DATETIME  NOT NULL,
-	`email` VARCHAR(255)  NOT NULL,
-	`letters_sent` INTEGER UNSIGNED NOT NULL DEFAULT 0,
-	`replys_completed` INTEGER UNSIGNED NOT NULL DEFAULT 0,
-	`replys_dropped` INTEGER UNSIGNED NOT NULL DEFAULT 0,
-	`status` ENUM( 'CONFIRMED', 'DORMANT', 'BAD_ADDRESS' ),
+	`joined` DATETIME  NOT NULL COMMENT 'Date of registration.',
+	`email` VARCHAR(255)  NOT NULL COMMENT 'Canonical e-mail address. All lowercased.',
+	`letters_sent` INTEGER UNSIGNED NOT NULL DEFAULT 0 COMMENT 'Just a counter of total letters initiated.',
+	`replys_completed` INTEGER UNSIGNED NOT NULL DEFAULT 0 COMMENT 'A counter of chains completed.',
+	`replys_dropped` INTEGER UNSIGNED NOT NULL DEFAULT 0 COMMENT 'A counter of chains dropped.',
+	`status` ENUM( 'CONFIRMED', 'DORMANT', 'BAD_ADDRESS' ) COMMENT 'Their current registration status.',
+	`spam_threshold` INTEGER NOT NULL DEFAULT 25 COMMENT 'Their taste for spam.',
+	`vulgarity_threshold` INTEGER NOT NULL DEFAULT 25 COMMENT 'Their taste for vulgarity.',
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `unique_emails`(`email`)
 ) ENGINE = MyISAM;
 
-'''
-Letters are always bound to chains. Chains are just a way of linking meta data about messages and replys.
-'''
+/*
+	Letters are always bound to chains. Chains are just a way of linking meta data about messages and replys.
+*/
 CREATE TABLE `letter_chains` (
 	`id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-	`slug` VARCHAR(40) NOT NULL,
-	`origin_id` INTEGER UNSIGNED NOT NULL,
-	`replier_id` INTEGER UNSIGNED DEFAULT NULL,
-	`started` DATETIME NOT NULL,
-	`linked` DATETIME DEFAULT NULL,
+	`slug` VARCHAR(40) NOT NULL COMMENT 'A unique slug for each email chain. A sha1 hash.',
+	`origin_id` INTEGER UNSIGNED NOT NULL COMMENT 'The users.id of the chain initator.',
+	`replier_id` INTEGER UNSIGNED DEFAULT NULL COMMENT 'The users.id of the chain replier.',
+	`started` DATETIME NOT NULL COMMENT 'When the chain was initiated.',
+	`linked` DATETIME DEFAULT NULL COMMENT 'When the chain was linked.',
+	`drop_count` INTEGER UNSIGNED NOT NULL DEFAULT 0 COMMENT 'How many times this chain has been dropped.',
+	`state` ENUM( 'PENDING', 'LINKED', 'FAILED', 'SPAM' ) COMMENT 'The state of this chain.',
 	PRIMARY KEY (`id`),
 	UNIQUE INDEX `unique_slugs`(`slug`)
 ) ENGINE = MyISAM;
 
-CREATE TABLE `letters` (
 
-) ENGINE = MyISAM;
-
-
-'''
-This is a table to track when un-linked chains are sent out to users
-as well as wether they complete them or drop them.
-'''
+/*
+	This is a table to track when un-linked chains are sent out to users
+	as well as wether they complete them or drop them.
+*/
 CREATE TABLE `attempted_links` (
-	`letter_id` INTEGER UNSIGNED NOT NULL,
-	`user_id` INTEGER UNSIGNED NOT NULL,
-	`link_sent` DATETIME NOT NULL,
-	`link_expires` DATETIME NOT NULL,
-	`link_result` ENUM( 'PENDING', 'LINKED', 'DROPPED', 'FAILED', 'VULGARED', 'SPAMMED' ),
-	`result_time` DATETIME DEFAULT NULL,
+	`letter_chain_id` INTEGER UNSIGNED NOT NULL COMMENT 'The ID of the chain we try to link.',
+	`user_id` INTEGER UNSIGNED NOT NULL COMMENT 'The users.id of the user we are trying to link with.',
+	`link_sent` DATETIME NOT NULL COMMENT 'When the link was sent.',
+	`link_expires` DATETIME NOT NULL COMMENT 'When the link will expire.',
+	`link_result` ENUM( 'PENDING', 'LINKED', 'DROPPED', 'FAILED', 'VULGARED', 'SPAMMED' ) COMMENT 'The end result of the link attempt.',
+	`result_time` DATETIME DEFAULT NULL COMMENT 'When the result occurred.',
 	PRIMARY KEY (`letter_id`, `user_id` ),
 	INDEX `expires_search` ( `link_expires`, `link_result` )
 ) ENGINE = MyISAM;
 
-'''
-A single letter sent to or from a stranger.
-We store the text of emails in two places, file names driven by the file hash of the FULL email.
-  1. Original, full text of the email (with headers) is stored in mail/full/[hash path]
-	2. Parsed, clean text of the email (that we send out) is stored in mail/clean/[hash path]
-'''
+/*
+	A single letter sent to or from a stranger.
+	We store the text of emails in two places, file names driven by the file hash of the FULL email.
+		1. Original, full text of the email (with headers) is stored in mail/full/[hash path]
+		2. Parsed, clean text of the email (that we send out) is stored in mail/clean/[hash path]
+*/
 CREATE TABLE `letters` (
 	`id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-	`letter_chain_id` INTEGER UNSIGNED NOT NULL,
-	`user_id` INTEGER UNSIGNED NOT NULL,
-	`hash` VARCHAR(40) NOT NULL,
-	`recieved` DATETIME NOT NULL,
+	`letter_chain_id` INTEGER UNSIGNED NOT NULL COMMENT 'The chain this letter belongs to.',
+	`user_id` INTEGER UNSIGNED NOT NULL COMMENT 'Who sent this letter.',
+	`hash` VARCHAR(40) NOT NULL COMMENT 'The hash of this letter. Determines file storage.',
+	`recieved` DATETIME NOT NULL COMMENT 'When the letter was recieved. For sorting.',
+	`spam_weight` INTEGER NOT NULL DEFAULT 0 COMMENT 'The likeliness that this letter is spam.',
+	`vulgarity_weight` INTEGER NOT NULL DEFAULT 0 COMMENT 'The amount of vulgarity in this letter.',
 	PRIMARY KEY ( `id` ),
 	INDEX `chain_index` ( `letter_chain_id` ),
-	INDEX `user_index` ( `user_id` )
+	INDEX `user_index` ( `user_id` ),
+	INDEX `sorting_index` ( `recieved_id` )
 ) ENGINE = MyISAM;
 
-'''
-This table documents all outbound stranger mail.
-'''
+/*
+	This table documents all outbound stranger mail.
+*/
 CREATE TABLE `letter_sends` (
 	`id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-	`letter_id` INTEGER UNSIGNED NOT NULL,
-	`user_id` INTEGER UNSIGNED NOT NULL,
-	`sent` DATETIME NOT NULL,
-	`failure` ENUM( 'NONE', 'BAD_ADDRESS' ),
-	`failure_time` DATETIME DEFAULT NULL,
+	`letter_id` INTEGER UNSIGNED NOT NULL COMMENT 'The ID of the letter.',
+	`sender_id` INTEGER UNSIGNED NOT NULL COMMENT 'Who sent the letter.',
+	`recipient_id` INTEGER UNSIGNED NOT NULL COMMENT 'Who recieved the letter.',
+	`sent` DATETIME NOT NULL COMMENT 'The time it was sent.',
+	`failure` ENUM( 'NONE', 'BAD_ADDRESS' ) COMMENT 'If it fails, how does it fail?',
+	`failure_time` DATETIME DEFAULT NULL COMMENT 'The time it fails, if at all.'
 	PRIMARY KEY ( `id` )
+) ENGINE = MyISAM;
+
+/*
+	This logs all mail counted as too vulgar by users
+*/
+CREATE TABLE `vulgar_log` (
+	`id` INTEGER UNSIGNED NOT NULL,
+	`letter_id` INTEGER UNSIGNED NOT NULL COMMENT 'The letter id.',
+	`marker_id` INTEGER UNSIGNED NOT NULL COMMENT 'The users.id that marked it as vulgar.',
+	`marked_at` DATETIME NOT NULL COMMENT 'When it was marked.',
+	PRIMARY KEY ( `id` ),
+	INDEX ( `letter_id` )
+) ENGINE = MyISAM;
+
+/*
+	This logs all mail counted as spam by users.
+*/
+CREATE TABLE `spam_log` (
+	`id` INTEGER UNSIGNED NOT NULL,
+	`letter_id` INTEGER UNSIGNED NOT NULL COMMENT 'The letter id.',
+	`marker_id` INTEGER UNSIGNED NOT NULL COMMENT 'The users.id that marked it as spam.',
+	`marked_at` DATETIME NOT NULL COMMENT 'When it was marked.',
+	PRIMARY KEY ( `id` ),
+	INDEX ( `letter_id` )
 ) ENGINE = MyISAM;
